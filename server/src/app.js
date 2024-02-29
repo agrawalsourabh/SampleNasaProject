cfd55aba44f8c92a6bf473c1fa5b5157ae1a676f
sourabh.agrawal <sourabh.agrawal@mati.io>
2024-02-29 18:09:42 +0530

    adding new endpoints

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,21 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
-const planetRouter = require('./routes/planets/planets.router');
+const morgan = require('morgan');
+const planetsRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
 
 const app = express();
 
 app.use(cors({
     origin: 'http://localhost:3000',
 }));
+app.use(morgan('combined'));
 app.use(express.json());
-app.use(planetRouter);
+
+app.use(launchesRouter);
+app.use(planetsRouter);
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 module.exports = app;
